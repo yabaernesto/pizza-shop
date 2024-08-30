e838698a41ae4646ba035e241d5bed136d866b99
Yaba Ernesto <yabaernesto@gmail.com>
2024-08-30 01:29:09 +0100

    feat: add header and theme

---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+import { Separator } from "@radix-ui/react-separator";
+import { Home, Pizza, UtensilsCrossed } from "lucide-react";
+import { NavLink } from "./nav-link";
+import { ThemeToggle } from "./theme/theme-toggle";
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Pizza className="h-6 w-6" />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-6 lg:space-x-6">
+          <NavLink to="/">
+            <Home className="h-4 w-4" />
+            Inicio
+          </NavLink>
+          <NavLink to="/orders">
+            <UtensilsCrossed className="h-4 w-4" />
+            Pedidos
+          </NavLink>
+        </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
+      </div>
+    </div>
+  );
+}
