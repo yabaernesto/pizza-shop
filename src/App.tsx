6968081dc57f5ca5064e0eebba023030a0cf0bd6
Yaba Ernesto <yabaernesto@gmail.com>
2024-08-30 01:29:28 +0100

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
 import './global.css';
+import { ThemeProvider } from './components/theme/theme-provider';
 
 export function App() {
 
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 };
